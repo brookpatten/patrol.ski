c6601977e0b4d37103a00cb0aabd04adfcb5230e
Brook Patten <brook@amphibiansoft.com>
2020-11-21 21:51:17 -0500

    added query and UI for trainer home

--- src/Amphibian.Patrol.Training.Web/src/views/schedule/Home.vue
+++ src/Amphibian.Patrol.Training.Web/src/views/schedule/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <CCard v-if="assignments.length>0">
+        <CCard v-if="myAssignments.length>0">
             <CCardHeader>
             <slot name="header">
                 <CIcon name="cil-grid"/> My Assignments
@@ -13,8 +13,50 @@
                 :bordered="true"
                 :small="small"
                 :fixed="fixed"
-                :items="assignments"
-                :fields="fields"
+                :items="myAssignments"
+                :fields="myAssignmentsFields"
+                :dark="dark">
+                <template #planName="data">
+                    <td><router-link :to="{ name: 'Assignment', params: {assignmentId:JSON.stringify(data.item.id)}}">{{data.item.planName}}</router-link></td>
+                </template>
+                <template #signatures="data">
+                    <td>{{data.item.signatures}}/{{data.item.signaturesRequired}}</td>
+                </template>
+                <template #progress="data">
+                    <td><CProgress
+                            :value="(Math.round((data.item.signatures / data.item.signaturesRequired) * 100))"
+                            color="success"
+                            striped
+                            :animated="animate"
+                            show-percentage
+                            />
+                    </td>
+                </template>
+                <template #dueAt="data">
+                    <td>{{data.item.dueAt ? (new Date(data.item.dueAt)).toLocaleDateString() : ''}}</td>
+                </template>
+                <template #assignedAt="data">
+                    <td>{{(new Date(data.item.assignedAt)).toLocaleDateString()}}</td>
+                </template>
+            </CDataTable>
+            </CCardBody>
+        </CCard>
+
+        <CCard v-if="trainerIncompleteAssignments.length>0">
+            <CCardHeader>
+            <slot name="header">
+                <CIcon name="cil-grid"/> Trainees with Incomplete Assignments
+            </slot>
+            </CCardHeader>
+            <CCardBody>
+            <CDataTable
+                :hover="hover"
+                :striped="true"
+                :bordered="true"
+                :small="small"
+                :fixed="fixed"
+                :items="trainerIncompleteAssignments"
+                :fields="trainerIncompleteAssignmentFields"
                 :dark="dark">
                 <template #planName="data">
                     <td><router-link :to="{ name: 'Assignment', params: {assignmentId:JSON.stringify(data.item.id)}}">{{data.item.planName}}</router-link></td>
@@ -49,33 +91,63 @@ export default {
   name: 'Home',
   components: {
   },
+  computed: {
+    selectedPatrolId: function () {
+      return this.$store.state.selectedPatrolId;
+    }
+  },
+  watch: {
+    selectedPatrolId(){
+      this.getIncompleteTrainerAssignments();
+    }
+  },
   props: [],
   data () {
     return {
         caption: '',
-        assignments: [],
-        fields:[
+        myAssignments: [],
+        trainerIncompleteAssignments:[],
+        myAssignmentsFields:[
           {key:'planName',label:''},
           {key:'assignedAt', label:'Assigned'},
           {key:'dueAt', label:'Due'},
           {key:'signatures', label:'Signatures'},
           {key:'progress', label:'Progress'}
-      ]
+        ],
+        trainerIncompleteAssignmentFields:[
+          {key:'userLastName',label:'Last'},
+          {key:'userFirstName',label:'First'},
+          {key:'planName',label:'Assignment'},
+          {key:'assignedAt', label:'Assigned'},
+          {key:'dueAt', label:'Due'},
+          {key:'signatures', label:'Signatures'},
+          {key:'progress', label:'Progress'}
+        ]
     }
   },
   methods: {
-        getAssignments(planId) {
+        getMyAssignments(planId) {
             this.$http.get('assignments')
                 .then(response => {
                     console.log(response);
-                    this.assignments = response.data;
+                    this.myAssignments = response.data;
+                }).catch(response => {
+                    console.log(response);
+                });
+        },
+        getIncompleteTrainerAssignments() {
+            this.$http.get('assignments/incomplete-for-trainer/'+this.selectedPatrolId)
+                .then(response => {
+                    console.log(response);
+                    this.trainerIncompleteAssignments = response.data;
                 }).catch(response => {
                     console.log(response);
                 });
         }
   },
   mounted: function(){
-      this.getAssignments(this.planId);
+      this.getMyAssignments(this.planId);
+      this.getIncompleteTrainerAssignments();
   }
 }
 </script>
