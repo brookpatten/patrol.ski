457d0a8f09acd49581a8649dc0c0a39e1012abd9
Brook Patten <brook@amphibiansoft.com>
2020-11-21 21:44:35 -0500

    added blur and cleaned up background video markup

--- src/Amphibian.Patrol.Training.Web/src/containers/PublicContainer.vue
+++ src/Amphibian.Patrol.Training.Web/src/containers/PublicContainer.vue
@@ -1,29 +1,49 @@
 <template>
   <div class="c-app">
-    <video autoplay muted loop id="backgroundVideo">
-        <source src="/snowmaking.mp4" type="video/mp4">
-    </video>
+    <div class="fullscreen-bg">
+      <video autoplay muted loop class="fullscreen-bg__video">
+          <source src="/snowmaking.mp4" type="video/mp4">
+      </video>
+    </div>
     <router-view></router-view>
   </div>
 </template>
 
 <style>
 @media (max-width: 767px) {
-    #backgroundVideo {
+    .fullscreen-bg__video {
         display: none;
     }
-    body {
+    .fullscreen-bg {
         background: url('/snowmaking.png');
         background-size: cover;
+        -webkit-filter: blur(5px);
+        -moz-filter: blur(5px);
+        -o-filter: blur(5px);
+        -ms-filter: blur(5px);
+        filter: blur(5px);
     }
   }
   
-#backgroundVideo {
+.fullscreen-bg__video {
     position: fixed;
     right: 0;
     bottom: 0;
     min-width: 100%;
     min-height: 100%;
+    -webkit-filter: blur(5px);
+    -moz-filter: blur(5px);
+    -o-filter: blur(5px);
+    -ms-filter: blur(5px);
+    filter: blur(5px);
+  }
+  .fullscreen-bg {
+    position: fixed;
+    right: 0;
+    bottom: 0;
+    min-width: 100%;
+    min-height: 100%;
+    overflow: hidden;
   }
 </style>
 
