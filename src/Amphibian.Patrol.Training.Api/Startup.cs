d12b6edc4b0c6fa8951be6e660bf4d8ce579f61b
Brook Patten <brook@amphibiansoft.com>
2020-11-21 21:45:04 -0500

    updated startup order

--- src/Amphibian.Patrol.Training.Api/Startup.cs
+++ src/Amphibian.Patrol.Training.Api/Startup.cs
@@ -23,6 +23,7 @@ using Amphibian.Patrol.Training.Api.Repositories;
 using System.Data;
 using Microsoft.Data.SqlClient;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
 
 namespace Amphibian.Patrol.Training.Api
 {
@@ -46,7 +47,7 @@ namespace Amphibian.Patrol.Training.Api
 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api", Version = "v1" });
             });
 
             services.AddAuthentication("BasicOrTokenAuthentication")
@@ -56,10 +57,10 @@ namespace Amphibian.Patrol.Training.Api
             {
                 return new SqlConnection(serviceConfiguration.Database.ConnectionString);
             });
-            services.AddScoped<UserRepository,UserRepository>();
-            services.AddScoped<TokenRepository, TokenRepository>();
-            services.AddScoped<Amphibian.Patrol.Training.Api.Services.AuthenticationService, Amphibian.Patrol.Training.Api.Services.AuthenticationService>();
-            services.AddScoped<Amphibian.Patrol.Training.Api.Services.PasswordService, Amphibian.Patrol.Training.Api.Services.PasswordService>(sp=>new Amphibian.Patrol.Training.Api.Services.PasswordService(5,32));
+            services.AddScoped<IUserRepository,UserRepository>();
+            services.AddScoped<ITokenRepository, TokenRepository>();
+            services.AddScoped<Amphibian.Patrol.Training.Api.Services.IAuthenticationService, Amphibian.Patrol.Training.Api.Services.AuthenticationService>();
+            services.AddScoped<Amphibian.Patrol.Training.Api.Services.IPasswordService, Amphibian.Patrol.Training.Api.Services.PasswordService>(sp=>new Amphibian.Patrol.Training.Api.Services.PasswordService(5,32));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -67,28 +68,31 @@ namespace Amphibian.Patrol.Training.Api
         {
 
             app.UseHttpsRedirection();
-            app.UseDefaultFiles(new DefaultFilesOptions() { 
-                DefaultFileNames = new List<string>() { "index.html" },
-                RequestPath=""
-            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseStaticFiles(new StaticFileOptions()
                 {
                     FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "../Amphibian.Patrol.Training.Web/dist")),
-                    RequestPath = ""
                 });
-                
+
             }
             else
             {
                 app.UseStaticFiles(new StaticFileOptions()
                 {
                     FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "static")),
-                    RequestPath = ""
                 });
             }
+            app.UseDefaultFiles();
+            
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseRouting();
 
@@ -100,16 +104,11 @@ namespace Amphibian.Patrol.Training.Api
                 .AllowAnyMethod()
                 .AllowAnyHeader());
 
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-            });
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+
         }
     }
 }
